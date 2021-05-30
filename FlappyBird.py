5d0e7c479644527af073a63f1d70f3f78920e547
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-30 07:36:06 -0300

    chore: create ground class with methods

--- FlappyBird.py
+++ FlappyBird.py
@@ -148,3 +148,26 @@ class Pipe:
             return False
 
 
+class Ground:
+    SPEED = 5
+    WIDTH = GROUND_IMAGE.get_width()
+    IMAGE = GROUND_IMAGE
+
+    def __init__(self, y_axis):
+        self.y_axis = y_axis
+        self.x_axis_ground_0 = 0
+        self.x_axis_ground_1 = self.WIDTH
+
+    def move(self):
+        self.x_axis_ground_0 -= self.SPEED
+        self.x_axis_ground_1 -= self.SPEED
+
+        if self.x_axis_ground_0 + self.WIDTH < 0:
+            self.x_axis_ground_0 = self.x_axis_ground_0 + self.WIDTH
+
+        if self.x_axis_ground_1 + self.WIDTH < 0:
+            self.x_axis_ground_1 = self.x_axis_ground_1 + self.WIDTH
+
+    def draw(self, screen):
+        screen.blit(self.IMAGE, (self.x_axis_ground_0, self.y_axis))
+        screen.blit(self.IMAGE, (self.x_axis_ground_1, self.y_axis))
